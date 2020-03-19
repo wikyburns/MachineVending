2d37a7fc0cd6c3052f6caf517c8624ffdc8613b6
Sebas <sebasbeje@gmail.com>
2020-03-19 16:39:11 +0100

    Create the FilePersistanceProductRepository implementing ProductRepository

---
+++ src/src/Product/Infrastructure/Persistance/FileProductRepository.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebas
+ * Date: 19/03/2020
+ * Time: 12:43
+ */
+declare(strict_types = 1);
+
+namespace App\Product\Infrastructure\Persistance;
+
+use App\Product\Domain\Product;
+use App\Product\Domain\ProductName;
+use App\Product\Domain\ProductRepository;
+
+class FileProductRepository implements ProductRepository
+{
+    private const FILE_PATH = __DIR__ . '/products';
+
+    public function findByName(ProductName $name): ?Product
+    {
+        return file_exists($this->fileName($name->value()))
+            ? unserialize(file_get_contents($this->fileName($name->value())))
+            : null;
+    }
+
+    public function save(Product $product): void
+    {
+        file_put_contents($this->fileName($product->name()->value()), serialize($product));
+    }
+
+    private function fileName(string $name): string
+    {
+        return sprintf('%s.%s.repo', self::FILE_PATH, $name);
+    }
+
+}
