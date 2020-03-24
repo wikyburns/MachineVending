dfe4ed69ed91c3c8f812510104a76304fab2e7a0
Sebas <sebasbeje@gmail.com>
2020-03-24 17:02:21 +0100

    BuyProduct controller

---
+++ src/src/Controller/Product/BuyProductGetController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sebas
+ * Date: 23/03/2020
+ * Time: 16:15
+ */
+
+namespace App\Controller\Product;
+
+use App\Coin\Infrastructure\Persistance\FileCoinRepository;
+use App\Product\Application\Buy\BuyProduct;
+use App\Product\Application\Buy\BuyProductRequest;
+use App\Product\Infrastructure\Persistance\FileProductRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class BuyProductGetController
+{
+
+    public function __invoke(Request $request)
+    {
+        $coins = $request->query->get('coins');
+        $product = $request->query->get('product');
+
+        $coinsExploded  = explode(',', $coins);
+
+        $requestDTO = new BuyProductRequest($product, ...$coinsExploded);
+        $useCase = new BuyProduct(new FileProductRepository(), new FileCoinRepository());
+
+        $result = $useCase->buy($requestDTO);
+
+        return new JsonResponse($result, 200);
+    }
+
+}
